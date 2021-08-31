52ad3f52bd3891d296c58733564609dc5d037c1c
kkedzierski <kamil.kedzierski@onet.com.pl>
2021-08-31 17:19:01 +0200

    create ReddiApi class with searchPosts and changeCategory functions

---
+++ src/api/RedditApi.js
@@ -0,0 +1,30 @@
+
+class RedditApi{
+
+
+    async searchPosts(query) {
+        let url = `https://www.reddit.com/search.json?q=${query}`;
+        const response = await fetch(url);
+        if (!response.ok) {
+            const message = `An error has occured: ${response.status}`;
+            throw new Error(message);
+        }
+        const posts = await response.json();
+        console.log(posts);
+        return posts;
+      }
+
+    async changeCategory(categoryName){
+        let url = `https://www.reddit.com/r/${categoryName}.json`;
+        const response = await fetch(url);
+        if (!response.ok) {
+            const message = `An error has occured: ${response.status}`;
+            throw new Error(message);
+        }
+        const posts = await response.json();
+        console.log(posts);
+        return posts;
+    }
+}
+
+export default RedditApi;
