287eba81ecf443cb1c732a054cb131f995c084fd
kkedzierski <kamil.kedzierski@onet.com.pl>
2021-09-04 13:12:24 +0200

    render all components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import Search from './features/search/Search';
+import Post from './features/posts/Post';
 import './App.css';
+import Subredit from './features/subredits/Subredit';
+import Comment from './features/comments/Comment'
 
 function App() {
   return (
+    <>
+      <Search />
+      <div class ="posts">
+        <Post />
+        <Comment />
+      </div>
+      <div class = "sidemenu subredits">
+        <Subredit />
+      </div>
+    </>
   );
 }
 
