b1fcbb957053096aac7d17c2b292483e32d67e37
kkedzierski <kamil.kedzierski@onet.com.pl>
2021-09-08 07:02:19 +0200

    use middlerware to make search and render result

--- src/features/posts/Post.js
+++ src/features/posts/Post.js
@@ -1,16 +1,36 @@
-
+import { useSelector } from "react-redux";
+import { selectSearchResult } from "../search/searchSlice"; 
 
 export default function Post() {
-    return(
-        <div>
-            <h2>Post Title</h2>
-            <img alt="post"/>
-            <button>nice</button>
-            <button>not nice</button>
-            <a href="#">post author</a>
-            <p>post date</p>
-            <button>show comments</button>
-        </div>
-        
-    )
+    let resultSearch = useSelector(selectSearchResult);
+    resultSearch = resultSearch.search.searchResult;
+    if(Object.keys(resultSearch).length !== 0 && typeof resultSearch !== 'undefined'){
+        return(
+            <div>
+                {resultSearch.data.children.map((post, i) => {
+                    return(
+                    <div key={i}>
+                            <h2>{post.data.title}</h2>
+                            <img src={post.data.url} alt="post"/>
+                            <button>nice</button>
+                            <button>not nice</button>
+                            <a href="/">{post.data.subreddit}</a>
+                            <p>{post.data.author}</p>
+                            
+                            <button>show comments</button>
+                        </div>
+                    )
+                        
+                    })
+                }
+            </div>
+        )
+    }else{
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+    }
+    
 }
