2cdc0fe9408bbd66bb8d1f7fe279a4156e1ea019
kkedzierski <kamil.kedzierski@onet.com.pl>
2021-09-11 20:14:18 +0200

    rebulid post component and create post apperance

--- src/features/posts/Post.js
+++ src/features/posts/Post.js
@@ -1,28 +1,48 @@
 import { useSelector } from "react-redux";
 import { selectSearchResult } from "../search/searchSlice"; 
+import { whenCreated } from "../../utilities/utilities";
+import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
+import styles from './Post.module.css';
 
 export default function Post() {
     let resultSearch = useSelector(selectSearchResult);
     resultSearch = resultSearch.search.searchResult;
+
+    let isPicture = false;
+    let createdAt = 0;
+
     if(Object.keys(resultSearch).length !== 0 && typeof resultSearch !== 'undefined'){
         return(
-            <div>
+            <div className={styles.container}>
                 {resultSearch.data.children.map((post, i) => {
+                    isPicture = post.data.url.slice(-4)[0] === '.' || post.data.url.slice(-5)[0] === '.' ? true : false;
+                    createdAt = whenCreated(post.data.created);
+                    console.log(post);
                     return(
-                    <div key={i}>
-                            <h2>{post.data.title}</h2>
-                            <img src={post.data.url} alt="post"/>
-                            <button>nice</button>
-                            <button>not nice</button>
-                            <a href="/">{post.data.subreddit}</a>
-                            <p>{post.data.author}</p>
-                            
-                            <button>show comments</button>
+                        <div className={styles.wrap}>
+                            <div className={styles.votes}>
+                                <FaArrowUp />
+                                <p className={styles.voteNumber}>{post.data.ups}</p>
+                                <FaArrowDown />
+                            </div>
+                            <div>   
+                                <div className={styles.postBody} key={i}>
+                                    <h3 className={styles.title}>{post.data.title}</h3>
+                                    {isPicture ?  <img 
+                                        className={styles.image} 
+                                        src={post.data.url} 
+                                        alt="post"
+                                    />: null}
+                                    <div className={styles.authorContainer}>
+                                        <a className={styles.subreddit} href="/">{post.data.subreddit}</a>
+                                        <p className={styles.author}>posted by {post.data.author}</p>
+                                        <p className={styles.createdAt}> created {createdAt} days ago</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     )
-                        
-                    })
-                }
+                })}
             </div>
         )
     }else{
