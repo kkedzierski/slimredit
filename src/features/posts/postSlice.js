ab92c94b1f14aa8d6848de0a1e8ab346be3ffc6f
kkedzierski <kamil.kedzierski@onet.com.pl>
2021-09-19 16:11:49 +0200

    add renderPostByPermalink function to render post detail add actions and reducers: render, remove post and status, add selector

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,16 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getPostDetail } from '../../api/RedditApi';
+
+export const renderPostByPermalink = createAsyncThunk(
+    'postDetail/renderPostDetail',
+    async (permalink, thunkAPI) =>{
+      const postDetail = await getPostDetail(permalink);
+      return postDetail;
+    }
+)
+
 
 const postSlice = createSlice({
-    name: 'posts',
+    name: 'postDetail',
     initialState: {
-        posts: {}
+        postDetail: {},
+        status: ''
     },
     reducers: {
-        renderPost: (state, action) => {
-
+        renderPostDetail: (state, action) => {
+            state.postDetail = action.payload;
+        },
+        removePostDetail: (state, action) => {
+            state.postDetail = {};
+        },
+        removePostStatus: (state, action) => {
+            state.status = ''
+        }
+    },
+    extraReducers: {
+        [renderPostByPermalink.pending]: (state, action) => {
+          state.status = 'loading';
+        },
+        [renderPostByPermalink.rejected]: (state, action) => {
+          state.status = 'error';
+        },
+        [renderPostByPermalink.fulfilled]: (state, action) => {
+          state.status = 'succeeded';
+          state.postDetail = action.payload;
         }
     }
 });
 
-export const {renderPost} = postSlice.actions;
+export const selectPostDetail = state => state = {
+    ...state,
+    postDetail: {
+        ...state.postDetail
+    }
+}
+
+export const {renderPostDetail, removePostDetail, removePostStatus} = postSlice.actions;
 export default postSlice.reducer;
