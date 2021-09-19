0c6a9da8417b0f92bccbc938982f4b1fbc9adbdd
kkedzierski <kamil.kedzierski@onet.com.pl>
2021-09-19 16:09:28 +0200

    rebulid Posts features

---
+++ src/features/posts/Posts.js
@@ -0,0 +1,61 @@
+import { useSelector, useDispatch } from "react-redux";
+import { selectSearchResult } from "../search/searchSlice"; 
+import { whenCreated, isPicture } from "../../utilities/utilities";
+import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
+import { renderPostByPermalink } from './postSlice.js';
+
+import styles from './Post.module.css';
+
+export default function Post() {
+    const dispatch = useDispatch();
+    let resultSearch = useSelector(selectSearchResult);
+    
+    resultSearch = resultSearch.search.searchResult;
+    let picture = false;
+    let createdAt = 0;
+    if(Object.keys(resultSearch).length !== 0 && typeof resultSearch !== 'undefined'){
+        return(
+            <div className={styles.container}>
+                {resultSearch.data.children.map((post, i) => {
+                    picture = isPicture(post.data.url);
+                    createdAt = whenCreated(post.data.created);
+                    
+                    return(
+                        <div className={styles.wrap} key={i}>
+                            <div className={styles.votes}>
+                                <FaArrowUp />
+                                <p className={styles.voteNumber}>{post.data.ups}</p>
+                                <FaArrowDown />
+                            </div>
+                            <div>   
+                                <div 
+                                    className={styles.postBody} 
+                                    key={i}
+                                    onClick = {() => dispatch(renderPostByPermalink(post.data.permalink))}
+                                >
+                                    <h3 className={styles.title}>{post.data.title}</h3>
+                                    {picture ?  <img 
+                                        className={styles.image} 
+                                        src={post.data.url} 
+                                        alt="post"
+                                    />: null}
+                                    <div className={styles.authorContainer}>
+                                        <a className={styles.subreddit} href="/">{post.data.subreddit}</a>
+                                        <p className={styles.author}>posted by {post.data.author}</p>
+                                        <p className={styles.createdAt}> created {createdAt} days ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }else{
+        return(
+            <div></div>
+        )
+    }
+    
+    
+}
